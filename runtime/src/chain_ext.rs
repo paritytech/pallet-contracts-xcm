8266d203e23902805884ea79c4164e876c86cdae
Hector Bulgarini <hbulgarini@gmail.com>
2022-10-31 14:25:53 +0100

    fmt

--- runtime/src/chain_ext.rs
+++ runtime/src/chain_ext.rs
@@ -1,177 +1,186 @@
-	use crate::{Config, Error as PalletError};
-	use codec::{Decode, Encode};
-	use frame_support::{weights::Weight, DefaultNoBound};
-	use pallet_contracts::chain_extension::{
-		ChainExtension, Environment, Ext, InitState, RegisteredChainExtension, Result, RetVal,
-		SysConfig, UncheckedFrom,
-	};
-	use sp_runtime::traits::Bounded;
-	use xcm::prelude::*;
-	use xcm_executor::traits::WeightBounds;
-	use log;
 
-	type RuntimeCallOf<T> = <T as SysConfig>::RuntimeCall;
+use crate::{Config, Error as PalletError};
+use codec::{Decode, Encode};
+use frame_support::{weights::Weight, DefaultNoBound};
+use log;
+use pallet_contracts::chain_extension::{
+	ChainExtension, Environment, Ext, InitState, RegisteredChainExtension, Result, RetVal,
+	SysConfig, UncheckedFrom,
+};
+use sp_runtime::traits::Bounded;
+use xcm::prelude::*;
+use xcm_executor::traits::WeightBounds;
 
-	#[repr(u16)]
-	#[derive(num_enum::TryFromPrimitive)]
-	enum Command {
-		PrepareExecute = 0,
-		Execute = 1,
-		ValidateSend = 2,
-		Send = 3,
-		NewQuery = 4,
-		TakeResponse = 5,
-	}
+type RuntimeCallOf<T> = <T as SysConfig>::RuntimeCall;
 
-	#[repr(u32)]
-	#[derive(num_enum::IntoPrimitive)]
-	enum Error {
-		Success = 0,
-		NoResponse = 1,
-	}
+#[repr(u16)]
+#[derive(num_enum::TryFromPrimitive)]
+enum Command {
+	PrepareExecute = 0,
+	Execute = 1,
+	ValidateSend = 2,
+	Send = 3,
+	NewQuery = 4,
+	TakeResponse = 5,
+}
 
-	#[derive(Decode)]
-	struct ValidateSendInput {
-		dest: VersionedMultiLocation,
-		xcm: VersionedXcm<()>,
-	}
+#[repr(u32)]
+#[derive(num_enum::IntoPrimitive)]
+enum Error {
+	Success = 0,
+	NoResponse = 1,
+}
 
-	pub struct PreparedExecution<Call> {
-		xcm: Xcm<Call>,
-		weight: Weight,
-	}
+#[derive(Decode)]
+struct ValidateSendInput {
+	dest: VersionedMultiLocation,
+	xcm: VersionedXcm<()>,
+}
 
-	pub struct ValidatedSend {
-		dest: MultiLocation,
-		xcm: Xcm<()>,
-	}
+pub struct PreparedExecution<Call> {
+	xcm: Xcm<Call>,
+	weight: Weight,
+}
 
-	#[derive(DefaultNoBound)]
-	pub struct Extension<T: Config> {
-		prepared_execute: Option<PreparedExecution<RuntimeCallOf<T>>>,
-		validated_send: Option<ValidatedSend>,
-	}
+pub struct ValidatedSend {
+	dest: MultiLocation,
+	xcm: Xcm<()>,
+}
 
-	macro_rules! unwrap {
-		($val:expr, $err:expr) => {
-			match $val {
-				Ok(inner) => inner,
-				Err(_) => return Ok(RetVal::Converging($err.into())),
-			}
-		};
-	}
+#[derive(DefaultNoBound)]
+pub struct Extension<T: Config> {
+	prepared_execute: Option<PreparedExecution<RuntimeCallOf<T>>>,
+	validated_send: Option<ValidatedSend>,
+}
 
-	impl<T: Config> ChainExtension<T> for Extension<T>
-	where
-		<T as SysConfig>::AccountId: AsRef<[u8; 32]>,
-	{
-		fn call<E>(&mut self, mut env: Environment<E, InitState>) -> Result<RetVal>
-		where
-			E: Ext<T = T>,
-			<E::T as SysConfig>::AccountId: UncheckedFrom<<E::T as SysConfig>::Hash> + AsRef<[u8]>,
-		{
-			match Command::try_from(env.func_id()).map_err(|_| PalletError::<T>::InvalidCommand)? {
-				Command::PrepareExecute => {
-					let mut env = env.buf_in_buf_out();
-					let len = env.in_len();
-					let input: VersionedXcm<RuntimeCallOf<T>> = env.read_as_unbounded(len)?;
-					let mut xcm =
-						input.try_into().map_err(|_| PalletError::<T>::XcmVersionNotSupported)?;
-					let weight =
-						Weight::from_ref_time(T::Weigher::weight(&mut xcm).map_err(|_| PalletError::<T>::CannotWeigh)?);
-					self.prepared_execute = Some(PreparedExecution { xcm, weight });
-					weight.using_encoded(|w| env.write(w, true, None))?;
-				},
-				Command::Execute => {
- 					let input =
-						self.prepared_execute.as_ref().take().ok_or(PalletError::<T>::PreparationMissing)?; 
-					env.charge_weight(input.weight)?;
-					let origin = MultiLocation {
-						parents: 0,
-						interior: Junctions::X1(Junction::AccountId32 {
-							network: Some(NetworkId::Rococo),
-							id: *env.ext().address().as_ref(),
-						}),
-					};
-					let hash = input.xcm.using_encoded(sp_io::hashing::blake2_256);
-					let outcome = T::XcmExecutor::execute_xcm_in_credit(
-						origin,
-						input.xcm.clone(),
-						hash,
-						input.weight.ref_time(),
-						input.weight.ref_time(),
-					);
-					// revert for anything but a complete excution
-					match outcome {
-						Outcome::Complete(_) => (),
-						_ => Err(PalletError::<T>::ExecutionFailed)?,
-					}
-				},
-				Command::ValidateSend => {
-					let mut env = env.buf_in_buf_out();
-					let len = env.in_len();
-					let input: ValidateSendInput = env.read_as_unbounded(len)?;
-					self.validated_send = Some(ValidatedSend {
-						dest: input
-							.dest
-							.try_into()
-							.map_err(|_| PalletError::<T>::XcmVersionNotSupported)?,
-						xcm: input
-							.xcm
-							.try_into()
-							.map_err(|_| PalletError::<T>::XcmVersionNotSupported)?,
-					});
-					// just a dummy asset until XCMv3 rolls around with its validate function
-					let asset = MultiAsset {
-						id: AssetId::Concrete(MultiLocation { parents: 0, interior: Junctions::Here }),
-						fun: Fungibility::Fungible(0),
-					};
-					VersionedMultiAsset::from(asset).using_encoded(|a| env.write(a, true, None))?;
-				},
-				Command::Send => {
-					let input =
-						self.validated_send.as_ref().take().ok_or(PalletError::<T>::PreparationMissing)?;
-					pallet_xcm::Pallet::<T>::send_xcm(Junctions::Here, input.dest, input.xcm.clone())
-						.map_err(|e| {
-							log::debug!(
-								target: "Contracts",
-								"Send Failed: {:?}",
-								e
-							);
-							PalletError::<T>::SendFailed
-						})?;
-				},
-				Command::NewQuery => {
-					let mut env = env.buf_in_buf_out();
-					let location = MultiLocation {
-						parents: 0,
-						interior: Junctions::X1(Junction::AccountId32 {
-							network: Some(NetworkId::Rococo),
-							id: *env.ext().address().as_ref(),
-						}),
-					};
-					let query_id: u64 =
-						pallet_xcm::Pallet::<T>::new_query(location, Bounded::max_value(),location).into();
-					query_id.using_encoded(|q| env.write(q, true, None))?;
-				},
-				Command::TakeResponse => {
-					let mut env = env.buf_in_buf_out();
-					let query_id: u64 = env.read_as()?;
-					let response = unwrap!(
-						pallet_xcm::Pallet::<T>::take_response(query_id).map(|ret| ret.0).ok_or(()),
-						Error::NoResponse
-					);
-					VersionedResponse::from(response).using_encoded(|r| env.write(r, true, None))?;
-				},
-			}
-
-			Ok(RetVal::Converging(Error::Success.into()))
+macro_rules! unwrap {
+	($val:expr, $err:expr) => {
+		match $val {
+			Ok(inner) => inner,
+			Err(_) => return Ok(RetVal::Converging($err.into())),
 		}
-	}
+	};
+}
 
-	impl<T: Config> RegisteredChainExtension<T> for Extension<T>
+impl<T: Config> ChainExtension<T> for Extension<T>
+where
+	<T as SysConfig>::AccountId: AsRef<[u8; 32]>,
+{
+	fn call<E>(&mut self, mut env: Environment<E, InitState>) -> Result<RetVal>
 	where
-		<T as SysConfig>::AccountId: AsRef<[u8; 32]>,
+		E: Ext<T = T>,
+		<E::T as SysConfig>::AccountId: UncheckedFrom<<E::T as SysConfig>::Hash> + AsRef<[u8]>,
 	{
-		const ID: u16 = 1;
+		match Command::try_from(env.func_id()).map_err(|_| PalletError::<T>::InvalidCommand)? {
+			Command::PrepareExecute => {
+				let mut env = env.buf_in_buf_out();
+				let len = env.in_len();
+				let input: VersionedXcm<RuntimeCallOf<T>> = env.read_as_unbounded(len)?;
+				let mut xcm =
+					input.try_into().map_err(|_| PalletError::<T>::XcmVersionNotSupported)?;
+				let weight = Weight::from_ref_time(
+					T::Weigher::weight(&mut xcm).map_err(|_| PalletError::<T>::CannotWeigh)?,
+				);
+				self.prepared_execute = Some(PreparedExecution { xcm, weight });
+				weight.using_encoded(|w| env.write(w, true, None))?;
+			},
+			Command::Execute => {
+				let input = self
+					.prepared_execute
+					.as_ref()
+					.take()
+					.ok_or(PalletError::<T>::PreparationMissing)?;
+				env.charge_weight(input.weight)?;
+				let origin = MultiLocation {
+					parents: 0,
+					interior: Junctions::X1(Junction::AccountId32 {
+						network: Some(NetworkId::Rococo),
+						id: *env.ext().address().as_ref(),
+					}),
+				};
+				let hash = input.xcm.using_encoded(sp_io::hashing::blake2_256);
+				let outcome = T::XcmExecutor::execute_xcm_in_credit(
+					origin,
+					input.xcm.clone(),
+					hash,
+					input.weight.ref_time(),
+					input.weight.ref_time(),
+				);
+				// revert for anything but a complete excution
+				match outcome {
+					Outcome::Complete(_) => (),
+					_ => Err(PalletError::<T>::ExecutionFailed)?,
+				}
+			},
+			Command::ValidateSend => {
+				let mut env = env.buf_in_buf_out();
+				let len = env.in_len();
+				let input: ValidateSendInput = env.read_as_unbounded(len)?;
+				self.validated_send = Some(ValidatedSend {
+					dest: input
+						.dest
+						.try_into()
+						.map_err(|_| PalletError::<T>::XcmVersionNotSupported)?,
+					xcm: input
+						.xcm
+						.try_into()
+						.map_err(|_| PalletError::<T>::XcmVersionNotSupported)?,
+				});
+				// just a dummy asset until XCMv3 rolls around with its validate function
+				let asset = MultiAsset {
+					id: AssetId::Concrete(MultiLocation { parents: 0, interior: Junctions::Here }),
+					fun: Fungibility::Fungible(0),
+				};
+				VersionedMultiAsset::from(asset).using_encoded(|a| env.write(a, true, None))?;
+			},
+			Command::Send => {
+				let input = self
+					.validated_send
+					.as_ref()
+					.take()
+					.ok_or(PalletError::<T>::PreparationMissing)?;
+				pallet_xcm::Pallet::<T>::send_xcm(Junctions::Here, input.dest, input.xcm.clone())
+					.map_err(|e| {
+					log::debug!(
+						target: "Contracts",
+						"Send Failed: {:?}",
+						e
+					);
+					PalletError::<T>::SendFailed
+				})?;
+			},
+			Command::NewQuery => {
+				let mut env = env.buf_in_buf_out();
+				let location = MultiLocation {
+					parents: 0,
+					interior: Junctions::X1(Junction::AccountId32 {
+						network: Some(NetworkId::Rococo),
+						id: *env.ext().address().as_ref(),
+					}),
+				};
+				let query_id: u64 =
+					pallet_xcm::Pallet::<T>::new_query(location, Bounded::max_value(), location)
+						.into();
+				query_id.using_encoded(|q| env.write(q, true, None))?;
+			},
+			Command::TakeResponse => {
+				let mut env = env.buf_in_buf_out();
+				let query_id: u64 = env.read_as()?;
+				let response = unwrap!(
+					pallet_xcm::Pallet::<T>::take_response(query_id).map(|ret| ret.0).ok_or(()),
+					Error::NoResponse
+				);
+				VersionedResponse::from(response).using_encoded(|r| env.write(r, true, None))?;
+			},
+		}
+
+		Ok(RetVal::Converging(Error::Success.into()))
 	}
+}
+
+impl<T: Config> RegisteredChainExtension<T> for Extension<T>
+where
+	<T as SysConfig>::AccountId: AsRef<[u8; 32]>,
+{
+	const ID: u16 = 1;
+}
